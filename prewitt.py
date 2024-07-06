e1019730445dc712181a4499138bff9cbc9f0e53
Jordan Chin <jc9627@g.rit.edu>
2024-07-06 14:35:43 -0400

    Added Prewitt and Laplacian

---
+++ prewitt.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+
+def gaussian(img):
+    img_gaussian = cv2.GaussianBlur(img, (3, 3), 0)
+    cv2.imshow("Gaussian kernel", img_gaussian)
+    cv2.waitKey(0)
+
+    return img_gaussian
+
+def main():
+    print("Prewitt Edge Detection Test")
+
+    img = cv2.imread('images/tomatoes.jpg')
+    cv2.imshow("Original Image", img)
+    cv2.waitKey(0)
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Grayscale Image", gray)
+    cv2.waitKey(0)
+
+    # Apply gaussian blur
+    img_gaussian = gaussian(gray)
+
+    # Apply Prewitt
+    # v = sobel(img_gaussian)
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
